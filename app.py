04384f1df6aa2c873f176f673085cf446caabfcf
Vikas Singh <singhvikas0610@gmail.com>
2023-12-03 10:43:28 +0530

    Calculator

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route('/')
-def index():
-    return render_template('first.html')
-@app.route('/sec')
-def second():
-    data=request.args.get("name")
-    return f"{data}"
-@app.route('/cr',methods=["post"])
-def third():
-    user=request.form["user"]
-    password=request.form.get("password")
-    print(user)
-    print(password)
-    return f"hello indians i am {user} and my password is {password}"
+def calcu():
+    return render_template("second.html")
+@app.route('/calc',methods=['post'])
+def calculator():
+    num1=int(request.form["num1"])
+    num2=int(request.form["num2"])
+    ops=request.form.get("operation")
+    if ops=="Add":
+        res=num1+num2
+    elif ops=="Sub":
+        res=num1-num2
+    elif ops=="Mul":
+        res=num1*num2
+    else:
+        res=num1/num2
+    return f"Result is {res}"
 if __name__=="__main__":
     app.run(debug=True)
